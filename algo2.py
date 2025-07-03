c5bc94cf32b6d06b70d303cab16e720ae0ac3f03
polinaartyukhova16092002 <99710229+polinaartyukhova16092002@users.noreply.github.com>
2025-07-03 15:11:18 +0300

    Add files via upload

--- algo2.py
+++ algo2.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import heapq
-from datetime import timedelta
+from datetime import timedelta, datetime
 import geopandas as gpd
 from typing import Dict, List, Optional, Tuple
+
+from numpy import sin, atan2, radians, sqrt, cos
 
 
 class Station:
@@ -20,7 +22,33 @@ class RailwayNetwork:
         self.stations: Dict[str, Station] = {}
         self.distance_matrix: List[List[Optional[float]]] = []
 
+
     def build_network(self, gdf: gpd.GeoDataFrame, routes: gpd.GeoDataFrame):
+        self.stations = {name: Station(name, idx) for idx, name in enumerate(gdf['name'].tolist())}
+
+        def haversine(lon1, lat1, lon2, lat2):
+            R = 6371  # Радиус Земли в км
+            lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+            dlon = lon2 - lon1
+            dlat = lat2 - lat1
+            a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+            c = 2 * atan2(sqrt(a), sqrt(1 - a))
+            return R * c
+
+        # Создаем матрицу расстояний
+        n = max([val.id for val in self.stations.values()]) + 1
+        self.distance_matrix = [[None] * n for _ in range(n)]
+
+        for i in range(n):
+            for j in range(n):
+                if i != j:
+                    coords_i = (gdf.iloc[i].geometry.x, gdf.iloc[i].geometry.y)
+                    coords_j = (gdf.iloc[j].geometry.x, gdf.iloc[j].geometry.y)
+                    self.distance_matrix[i][j] = haversine(*coords_i, *coords_j)
+                else:
+                    self.distance_matrix[i][j] = 0
+
+    def build_network2(self, gdf: gpd.GeoDataFrame, routes: gpd.GeoDataFrame):
         """Строит сеть станций и матрицу расстояний"""
         # Создаем станции
         self.stations = {name: Station(name, idx) for idx, name in enumerate(gdf['name'].tolist())}
@@ -120,7 +148,7 @@ class LocomotiveDispatcher:
     def process_routes(self, routes_df: pd.DataFrame):
         """Обрабатывает все маршруты"""
         # Сортируем маршруты по времени отправления
-        sorted_routes = routes_df.sort_values('arrival_min')
+        sorted_routes = routes_df.sort_values(['departure_min', 'arrival_min'])
 
         for _, row in sorted_routes.iterrows():
             route = TrainRoute(
@@ -129,8 +157,9 @@ class LocomotiveDispatcher:
                 from_station=row['from'],
                 to_station=row['to'],
                 departure_min=row['departure_min'],
-                travel_duration_min=row['arrival_min'] - row['departure_min']
+                travel_duration_min=row['travel_time_min']
             )
+            # print("TTT: " + str(row['travel_time_min']) + str(row['departure_min']) +" " +  str(row['arrival_min']))
             self._assign_locomotive(route)
 
     def _assign_locomotive(self, route: TrainRoute):
@@ -140,6 +169,7 @@ class LocomotiveDispatcher:
 
         # Ищем подходящий локомотив
         best_loco, best_distance = self._find_best_locomotive(route)
+        # print(best_distance) #############
 
         if best_loco:
             # Записываем назначение
@@ -155,6 +185,7 @@ class LocomotiveDispatcher:
                            (best_loco.available_time_min, best_loco.id, best_loco))
         else:
             print(f"Не найден локомотив типа {route.loco_type} для поезда {route.train_number}")
+            self._record_assignment(route, None, 0)
             self.errors_count += 1
 
     def _release_completed_locomotives(self, current_time_min: int):
@@ -180,28 +211,28 @@ class LocomotiveDispatcher:
                 distance = 0 if loco.current_station is None else \
                     self.network.get_distance(loco.current_station, route.from_station) or float('inf')
 
-                cost = distance * self.cost_per_km[loco.type]
+                cost = distance
                 if cost < min_cost:
-                    min_cost = cost
+                    min_cost = cost * self.cost_per_km[loco.type]
                     best_loco = loco
                     best_distance = distance
-        if (min_cost == float('inf')):
-            print(route.pretty_print())
+        # if (min_cost == float('inf')):
+        #     print(route.pretty_print())
         return best_loco, best_distance
 
     def _record_assignment(self, route: TrainRoute, loco: Locomotive, distance: float):
         """Записывает информацию о назначении"""
         self.assignments.append({
             'train_number': route.train_number,
-            'locomotive': loco.id,
+            'locomotive': loco.id if loco else None,
             'from': route.from_station,
             'to': route.to_station,
             'departure_time': self._minutes_to_time(route.departure_min),
             'arrival_time': self._minutes_to_time(route.departure_min + route.travel_duration_min),
             'travel_time_min': route.travel_duration_min,
             'empty_run_km': distance,
-            'empty_run_cost': distance * self.cost_per_km[loco.type],
-            'previous_station': loco.current_station
+            'empty_run_cost': distance * self.cost_per_km[loco.type] if loco else 0,
+            'previous_station': loco.current_station if loco else None
         })
 
     @staticmethod
@@ -212,7 +243,6 @@ class LocomotiveDispatcher:
     def get_results(self) -> pd.DataFrame:
         """Возвращает результаты в виде DataFrame"""
         return pd.DataFrame(self.assignments)
-
 
 # Функции для работы с временем
 def time_to_minutes(time_obj) -> int:
@@ -233,13 +263,125 @@ def time_to_minutes(time_obj) -> int:
             return 0
     if isinstance(time_obj, timedelta):
         return int(time_obj.total_seconds() / 60)
+    if isinstance(time_obj, datetime):
+        return time_obj.hour * 60 + time_obj.minute + (1 if time_obj.second >= 30 else 0)
+        # Если время в формате datetime.time
+    elif hasattr(time_obj, 'hour'):  # Для datetime.time или datetime.datetime
+        return round(time_obj.hour * 60 + time_obj.minute + time_obj.second / 60)
     return 0
+
+
+def calculate_objective_function(result: pd.DataFrame, park_df, railway_network: RailwayNetwork) -> float:
+    """
+    Вычисляет целевую функцию для заданных результатов распределения локомотивов
+
+    """
+    ERROR_VALUE = 500_000
+    results = result.copy()
+
+    cost_per_km = park_df.set_index('Название л-ва')['Стоимость порожнего хода (р/км)'].to_dict()
+    cost_per_min = park_df.set_index('Название л-ва')['Стоимость простоя (р/мин)'].to_dict()
+
+    # количество ошибок
+    nan_count = results['locomotive'].isna().sum()
+
+    # Сортируем по локомотивам и времени отправления
+    results_sorted = results.sort_values(['locomotive', 'departure_time'])
+
+    results_sorted['next_from'] = results_sorted.groupby('locomotive')['from'].shift(-1)
+    results_sorted['empty_run_distance'] = results_sorted.apply(
+        lambda row: railway_network.get_distance(row['to'], row['next_from'])
+        if pd.notna(row['next_from']) else 0,
+        axis=1
+    )
+
+
+    grouped = results_sorted.groupby('locomotive')
+    value_ans = 0
+    km_check = 0
+    # Для каждой группы (каждого локомотива) вычисляем время простоя и расстояние перегонки
+    for name, group in grouped:
+        if name is not None:
+            bad_times = group['travel_time_min'].sum()
+            total_empty_distance = group['empty_run_distance'].sum()
+            # print(bad_times)
+
+            value_ans += (24 * 60 - bad_times) * cost_per_min[name.split("_")[0]] + total_empty_distance * cost_per_km[name.split("_")[0]]
+            km_check += total_empty_distance
+
+    for loc in cost_per_km.keys():
+        x = park_df.loc[park_df['Название л-ва'] == loc, 'Кол-во в парке'].values[0]
+        for i in range(1, x + 1):
+            name = loc + "_" + str(i)
+            if name not in results['locomotive'].values:
+                value_ans += 24 * 60 * cost_per_min[loc]
+
+
+    value_ans += nan_count * ERROR_VALUE
+
+    return value_ans
+
+
+
+def calculate_objective_function_print(result: pd.DataFrame, park_df, railway_network: RailwayNetwork) -> float:
+    """
+    Вычисляет целевую функцию для заданных результатов распределения локомотивов
+
+    """
+    ERROR_VALUE = 500_000
+    results = result.copy()
+
+    cost_per_km = park_df.set_index('Название л-ва')['Стоимость порожнего хода (р/км)'].to_dict()
+    cost_per_min = park_df.set_index('Название л-ва')['Стоимость простоя (р/мин)'].to_dict()
+
+    # количество ошибок
+    nan_count = results['locomotive'].isna().sum()
+    print("count: " + str(nan_count))
+
+    # Сортируем по локомотивам и времени отправления
+    results_sorted = results.sort_values(['locomotive', 'departure_time'])
+
+    results_sorted['next_from'] = results_sorted.groupby('locomotive')['from'].shift(-1)
+    results_sorted['empty_run_distance'] = results_sorted.apply(
+        lambda row: railway_network.get_distance(row['to'], row['next_from'])
+        if pd.notna(row['next_from']) else 0,
+        axis=1
+    )
+
+
+    grouped = results_sorted.groupby('locomotive')
+    value_ans = 0
+    km_check = 0
+    # Для каждой группы (каждого локомотива) вычисляем время простоя и расстояние перегонки
+    for name, group in grouped:
+        if name is not None:
+            bad_times = group['travel_time_min'].sum()
+            total_empty_distance = group['empty_run_distance'].sum()
+            # print(bad_times)
+
+            value_ans += (24 * 60 - bad_times) * cost_per_min[name.split("_")[0]] + total_empty_distance * cost_per_km[name.split("_")[0]]
+            km_check += total_empty_distance
+
+    for loc in cost_per_km.keys():
+        x = park_df.loc[park_df['Название л-ва'] == loc, 'Кол-во в парке'].values[0]
+        for i in range(1, x + 1):
+            name = loc + "_" + str(i)
+            if name not in results['locomotive'].values:
+                value_ans += 24 * 60 * cost_per_min[loc]
+                print("простой: " + name)
+
+
+    value_ans += nan_count * ERROR_VALUE
+    print(value_ans, "км перегонки: " +  str(km_check))
+
+    return value_ans
+
 
 
 
 # Загрузка данных
-routes_df = pd.read_excel('routes (2).xlsx', sheet_name='Маршруты')
-park_df = pd.read_excel('routes (2).xlsx', sheet_name='Парк')
+routes_df = pd.read_excel('routes (3).xlsx', sheet_name='Маршруты')
+park_df = pd.read_excel('routes (3).xlsx', sheet_name='Парк')
 routs_all = gpd.read_file("all_routes_v2.shp")
 gdf = gpd.read_file("selected1.shp")
 
@@ -271,67 +413,42 @@ print()
 print("ERRORS count = " + str(dispatcher.errors_count))
 print(f"Суммарный порожний пробег: {total_empty_km:.2f} км")
 print(f"Суммарная стоимость порожнего пробега: {total_empty_cost:.2f} руб.")
-print("\nПервые 10 назначений:")
-print(results.head(10)[['train_number', 'locomotive', 'from', 'to',
-                        'departure_time', 'arrival_time', 'empty_run_km']])
+# print(results.head(10)[['train_number', 'locomotive', 'from', 'to',
+#                         'departure_time', 'arrival_time', 'empty_run_km']])
+
+
+# # созранени в exel
+# output_file = "result_data.xlsx"  # Имя файла для сохранения
+#
+# result = results.copy()
+# # Основное сохранение
+# result.to_excel(
+#     output_file,
+#     index=False,          # Не включать индекс в файл
+#     sheet_name='Результаты',  # Название листа
+#
+# )
+#
+# print(f"Данные успешно сохранены в файл {output_file}")
+
+
+
+
 
 # dispatcher = LocomotiveDispatcher(railway_network)
 # dispatcher.initialize_locomotives(park_df)
 # # Обрабатываем маршруты
 # dispatcher.process_routes(routes_df)
 # results = dispatcher.get_results()
-#
-
-# print()
-# print("ERRORS count = " + str(dispatcher.errors_count))
-# print(f"Суммарный порожний пробег: {total_empty_km:.2f} км")
-# print(f"Суммарная стоимость порожнего пробега: {total_empty_cost:.2f} руб.")
-print("\nПервые 10 назначений:")
-print(results.head(10)[['train_number', 'locomotive', 'from', 'to',
-                        'departure_time', 'arrival_time', 'empty_run_km']])
 
 
-def calculate_objective_function(result: pd.DataFrame, cost_per_empty_km: Dict[str, float],
-                                 cost_per_min: Dict[str, float], railway_network: RailwayNetwork) -> float:
-    """
-    Вычисляет целевую функцию для заданных результатов распределения локомотивов
+goal_value = calculate_objective_function(results, park_df, railway_network)
+print("ЦЕЛЕВАЯ f: " + str(goal_value))
 
-    """
-    ERROR_VALUE = 100
-    results = result.copy()
+# это если хотим вывести матрицу расстояний
+# for station in railway_network.stations.keys():
+#     for station2 in railway_network.stations.keys():
+#         print(station + " -> " + station2 +" " + str(railway_network.get_distance(station, station2)))
 
-    # Сортируем по локомотивам и времени отправления
-    results_sorted = results.sort_values(['locomotive', 'departure_time'])
-
-    results_sorted['next_from'] = results_sorted.groupby('locomotive')['from'].shift(-1)
-    results_sorted['empty_run_distance'] = results_sorted.apply(
-        lambda row: railway_network.get_distance(row['to'], row['next_from'])
-        if pd.notna(row['next_from']) else 0,
-        axis=1
-    )
-
-
-    grouped = results_sorted.groupby('locomotive')
-    value_ans = 0
-    km_check = 0
-    # Для каждой группы (каждого локомотива) вычисляем время простоя
-    for name, group in grouped:
-        if name != None:
-            bad_times = group['travel_time_min'].sum()
-            total_empty_distance = group['empty_run_distance'].sum()
-            print(bad_times)
-
-            value_ans += (24 * 60 - bad_times) * cost_per_min[name.split("_")[0]] + total_empty_distance * cost_per_empty_km[name.split("_")[0]]
-            km_check += total_empty_distance
-
-    nan_count = results['locomotive'].isna().sum()
-    value_ans += nan_count * ERROR_VALUE
-
-    print(value_ans, km_check)
-
-    return value_ans
-
-cost_per_km = park_df.set_index('Название л-ва')['Стоимость порожнего хода (р/км)'].to_dict()
-cost_per_min = park_df.set_index('Название л-ва')['Стоимость простоя (р/мин)'].to_dict()
-calculate_objective_function(results, cost_per_km, cost_per_min, railway_network)
-
+# 15072565.255440742
+# 18399799
